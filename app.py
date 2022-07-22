4a739591b758151248eb1be30cb115af82ef24cb
unknown <ck220698@gmail.com>
2022-07-22 15:18:51 +0530

    new changes

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
+import sys
 from housing.logger import logging
+from housing.exception import HousingException
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
-    logging.info("We are testing logging module")
+    try:
+        raise Exception("We are testing the exception")
+    except Exception as e:
+        housing_object = HousingException(e, sys)
+        logging.info(housing_object.error_message)
+        logging.info("We are testing logging module")
+        
     return "Hello World !"
 
 if __name__ == "__main__":
